c2b3989ba9e86ca8c75e6f21adaccd79444160f1
Rony Azrieh <94086447+ronyazrieh@users.noreply.github.com>
2024-03-13 11:43:51 -0400

    Started select muscle group and workout page

--- LMTapp/homepage/urls.py
+++ LMTapp/homepage/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, workout, sample_workout
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('home', home, name='home'),
+    path('workout/', sample_workout, name='sample_workout'),
+    path('workout/<str:muscle>/', workout, name='workout'),
 ]
--- LMTapp/homepage/views.py
+++ LMTapp/homepage/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'homepage/home.html')
+
+def sample_workout(request):
+    return render(request, 'sample_workout.html')
+
+def workout(request, muscle):
+    context = {'muscle': muscle}
+    return render(request, 'workout.html', context)
