37758f07a6f563150232cf7697b124d90b9862bb
Rony Azrieh <94086447+ronyazrieh@users.noreply.github.com>
2024-03-13 13:35:34 -0400

    Default page is also home

--- LMTapp/homepage/urls.py
+++ LMTapp/homepage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home, sample_workout, chest_workout, back_workout
 
 urlpatterns = [
+    path('', home, name='default_home'),
     path('home', home, name='home'),
     path('workout/', sample_workout, name='sample_workout'),
      path('workout/chest/', chest_workout, name='chest_workout'),
