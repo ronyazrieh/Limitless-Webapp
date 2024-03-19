28c2c0fcba39cf248cf49b4599dc9443480bd6ac
Rony Azrieh <94086447+ronyazrieh@users.noreply.github.com>
2024-03-18 21:59:50 -0400

    New pages for muscles!

--- LMTapp/homepage/urls.py
+++ LMTapp/homepage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, sample_workout, chest_workout, back_workout
+from .views import home, sample_workout, chest_workout, back_workout, bicep_workout, tricep_workout, delts_workout, quads_workout, hamstring_workout, glutes_workout
 
 urlpatterns = [
     path('', home, name='default_home'),
@@ -7,4 +7,10 @@ urlpatterns = [
     path('workout/', sample_workout, name='sample_workout'),
      path('workout/chest/', chest_workout, name='chest_workout'),
     path('workout/back/', back_workout, name='back_workout'),
+    path('workout/bicep/', bicep_workout, name='bicep_workout'),
+    path('workout/tricep/', tricep_workout, name='tricep_workout'),
+    path('workout/delts/', delts_workout, name='delts_workout'),
+    path('workout/quads/', quads_workout, name='quads_workout'),
+    path('workout/hamstring/', hamstring_workout, name='hamstring_workout'),
+    path('workout/glutes/', glutes_workout, name='glutes_workout'),
 ]
